6f4d32339675477311a6ed907355dfb3e7835b79
Alieke <aliekevanderlelie@hotmail.com>
2015-10-27 09:49:05 +0100

    Bezig met puzzel
    PUZZEL

--- Implemented/Order66/Assets/Scripts/LevelScripts/SpiderWeb.cs
+++ Implemented/Order66/Assets/Scripts/LevelScripts/SpiderWeb.cs
@@ -5,7 +5,7 @@ public class SpiderWeb : MonoBehaviour {
 	private RaycastHit hit;
 	public float rayDis;
 	public Rigidbody rb;
-	public int jumpHeight;
+	public float jumpHeight;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
@@ -14,8 +14,18 @@ public class SpiderWeb : MonoBehaviour {
 	void Update () {
 		if (Physics.Raycast(transform.position, -Vector3.up, out hit, rayDis)){
 			if(hit.transform.tag == "SpiderWeb"){
+
 				rb.velocity = new Vector3 (0, jumpHeight, 0);
 			}
 		}
 	}
 }
+	/*
+	void OnCollisionEnter (Collision player){
+		if(player.transform.tag == "Player"){
+			rb = player.gameObject.GetComponent<Rigidbody>();
+			rb.velocity = new Vector3 (0, jumpHeight, 0);
+		}
+	}
+}
+*/
