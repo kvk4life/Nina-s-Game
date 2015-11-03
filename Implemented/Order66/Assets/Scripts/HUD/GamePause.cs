20a201541b26128a1bba822c7c682727c5d76d20
Alieke <aliekevanderlelie@hotmail.com>
2015-11-03 16:56:24 +0100

    Pause menu verbeterd
    PLEH

--- Implemented/Order66/Assets/Scripts/HUD/GamePause.cs
+++ Implemented/Order66/Assets/Scripts/HUD/GamePause.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class GamePause : MonoBehaviour {
 	public GameObject pauseMenu;
+	public GameObject controlPanel;
+	public GameObject optionsMenu;
+	public GameObject loadMenu;
 
 	void Update (){
 		if(Input.GetButtonDown("Esc")){
@@ -13,6 +16,32 @@ public class GamePause : MonoBehaviour {
 	public void  UnPause () {
 		Time.timeScale = 1f;
 		pauseMenu.SetActive(false);
+	}
+	public void BackToMainMenu (){
+		Application.LoadLevel ("Main Menu");
+		Time.timeScale = 1f;
+	}
+
+	public void Controls (){
+		controlPanel.SetActive (true);
+		pauseMenu.SetActive (false);
+	}
+
+	public void Return (){
+		optionsMenu.SetActive (false);
+		loadMenu.SetActive (false);
+		controlPanel.SetActive (false);
+		pauseMenu.SetActive (true);
+	}
+
+	public void Options (){
+		optionsMenu.SetActive (true);
+		pauseMenu.SetActive (false);
+	}
+
+	public void LoadGames (){
+		pauseMenu.SetActive(false);
+		loadMenu.SetActive(true);
 	}
 }
 
