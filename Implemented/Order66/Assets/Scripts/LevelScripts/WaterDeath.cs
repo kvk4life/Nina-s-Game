c704faed97c8266f1e2e11fccf965a143aeeba56
Alieke <aliekevanderlelie@hotmail.com>
2015-11-04 16:29:47 +0100

    probeer de blaadjes te laten werken
    ^ja dat

--- Implemented/Order66/Assets/Scripts/LevelScripts/WaterDeath.cs
+++ Implemented/Order66/Assets/Scripts/LevelScripts/WaterDeath.cs
@@ -2,13 +2,13 @@
 using System.Collections;
 
 public class WaterDeath : MonoBehaviour {
-	public GameObject player;
-
-	void OnCollisionEnter(Collision col){
+	
+	void OnTriggerEnter(Collider col){
+		print (col);
 		//Wanneer de DeathBlock, een ontzichtbaar G.O. boven het hoofd van Nina
 		//In het water terecht komt is Nina instakillt
-		if(col.transform.tag == "DeathBlock"){
-			player.GetComponent<PlayerHealth> ().health = 0;
+		if(col.transform.tag == "Water"){
+			GetComponent<PlayerHealth> ().health = 0;
 		}
 	}
 }
