131b049575020a73b34d2ac3109358ade1d507e1
HaroldGaming <harold-de-jong@hotmail.com>
2016-01-25 10:06:56 +0100

    Animaties stuff
    ANIMATIES HIER
    missen een paar, maawr fuck it -mack 2016

---
+++ Implemented/Order66/Assets/Scripts/HaroldsScripts/Animaties/Play_Animation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play_Animation : MonoBehaviour 
+{
+	public Animator animation;	
+
+
+	void Move(float moveSpeed)
+	{
+		//1 = walk, 2 = running
+		animation.SetFloat("moveSpeed", moveSpeed);
+	}
+
+	public void Death()
+	{
+		animation.SetBool("death", true);
+	}
+		
+	public void PickUp()
+	{
+		animation.SetTrigger("pickUp");
+	}
+
+	public void Gaurd()
+	{
+		animation.SetBool("guard", true);
+	}
+
+	public void StopGuard()
+	{
+		animation.SetBool("guard", false);
+	}
+
+	public void Attack(float attack)
+	{
+		//0 = first attack, 1 = second attack, 2 is third attack
+		animation.SetFloat("moveSpeed", attack);
+	}
+}
