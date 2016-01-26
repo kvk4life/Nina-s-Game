b35611d1270a5ef421841a49ac85458d2d60f8ed
M. Mendes Moreira <kcamvankester@hotmail.com>
2016-01-26 15:51:12 +0100

    Heb geluid werkend
    Nu moeten we alleen de juiste audio clips op de juiste trigger velden
    geven. Maar ahrold gaat even de goede animation erop zetten

--- Implemented/Order66/Assets/Scripts/LevelScripts/TextTrigger.cs
+++ Implemented/Order66/Assets/Scripts/LevelScripts/TextTrigger.cs
@@ -2,16 +2,32 @@
 using System.Collections;
 
 public class TextTrigger : MonoBehaviour {
+	private AudioSource leSource;
+	public AudioClip myClip;
 	public int balloonTextMin;
 	public int balloonTextMax;
 	private GameObject subtitleHolder;
+
+	void Start(){
+		leSource = GameObject.FindWithTag ("Player").GetComponent<AudioSource>();
+	}
 	
 	public void OnTriggerEnter(Collider col){
 		if(col.transform.tag == "Player"){
 			subtitleHolder = GameObject.Find ("SubtitleMng");
+			ClipChecker();
 			subtitleHolder.GetComponent<Subtitles>().triggerSubtitles = true;
 			subtitleHolder.GetComponent<Subtitles>().curBalloonText = balloonTextMin;
 			subtitleHolder.GetComponent<Subtitles>().curBalloonTextMax = balloonTextMax;
+		}
+	}
+
+	void ClipChecker(){
+		if (myClip != null) {
+			leSource.PlayOneShot (myClip);
+		}
+		else {
+			return;
 		}
 	}
 }
